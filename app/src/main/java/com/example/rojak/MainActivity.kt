c9d107cecdbc4c08a649f1638bc9d3b51d95ce5a
ptvrajsk <ptvrajsk@gmail.com>
2019-11-01 15:05:19 +0800

    Basic SplashPage and HomePage
    Basic SplashPage with font and Fade Animations as well as a Skeleton
    Homepage has been instantiated.

--- app/src/main/java/com/example/rojak/MainActivity.kt
+++ app/src/main/java/com/example/rojak/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.rojak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val SPLASH_TIMEOUT: Long = 1500
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val gotoHome: Intent = Intent(this, HomeActivity::class.java)
+            startActivity(gotoHome)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, SPLASH_TIMEOUT)
     }
 }
