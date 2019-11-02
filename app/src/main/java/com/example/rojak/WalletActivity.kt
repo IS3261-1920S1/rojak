312e10232c850d1b5f08b75fe279f66f54b3a105
Prithivi Raj <ptvrajsk@gmail.com>
2019-11-02 15:11:37 +0800

    WalletPage

---
+++ app/src/main/java/com/example/rojak/WalletActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rojak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WalletActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet)
+    }
+}
