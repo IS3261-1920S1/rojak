18671a7f6b12643fa85e90864a20d30c8b2047d9
ptvrajsk <ptvrajsk@gmail.com>
2019-11-04 00:36:21 +0800

    Removing Unecessary Import

--- app/src/main/java/com/example/rojak/activities/home/HomeActivity.kt
+++ app/src/main/java/com/example/rojak/activities/home/HomeActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.Button
 import com.example.rojak.R
 import com.example.rojak.activities.wallet.WalletActivity
-import com.example.rojak.database.DatabaseHelper
 
 class HomeActivity : AppCompatActivity() {
 
